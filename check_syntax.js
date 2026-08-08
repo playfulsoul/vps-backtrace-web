f1092089dc4cf6d181ff160441385e8c35da2622
playful <45899776+nomad70@users.noreply.github.com>
2026-08-08 17:38:26 +0800

    Delete check_syntax.js

--- check_syntax.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('index.html', 'utf8');
-const js = html.substring(html.indexOf('<script>') + 8, html.lastIndexOf('</script>'));
-try {
-  new Function(js);
-  console.log("Syntax OK");
-} catch (e) {
-  console.error("Syntax Error:", e);
-}
